b52313f401feb7204a453e2b5234c94c51bfd385
kontopro <konteap@gmail.com>
2019-03-19 23:45:38 +0200

    added dotenv

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,6 +1,10 @@
+require("dotenv").config({
+  path: `.env.${process.env.NODE_ENV}`,
+})
+
 module.exports = {
   siteMetadata: {
-    title: `Prokopis Gatsby Default Starter`,
+    title: `Prokopis Gatsby Blog App`,
     description: `My next, prokopis great Gatsby project`,
     author: `kontopro`,
   },
