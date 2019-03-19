51ed2d2614bdc885e2fbce3dc255907269755153
kontopro <konteap@gmail.com>
2019-03-19 22:48:40 +0200

    gatsby env variables

--- src/components/base.js
+++ src/components/base.js
@@ -3,9 +3,9 @@ import firebase from "firebase/app";
 import 'firebase/database';
 
 const firebaseApp = firebase.initializeApp({
-  apiKey: "AIzaSyD3u34u7Hi4tkxpqVWoXJzYDjDuxNvTdDM",
-  authDomain: "playgatsby.firebaseapp.com",
-  databaseURL: "https://playgatsby.firebaseio.com"
+  apiKey: process.env.GATSBY_API_KEY,
+  authDomain: process.env.GATSBY_AUTH_DOMAIN,
+  databaseURL: process.env.GATSBY_DATABASE_URL
 });
 
 const base = Rebase.createClass(firebaseApp.database());
