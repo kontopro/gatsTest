1a16c32529881a43623396ba25873caa153a32ba
kontopro <konteap@gmail.com>
2019-03-19 17:22:59 +0200

    new start

---
+++ src/pages/app-simple.js
@@ -0,0 +1,22 @@
+import React, { Component } from "react";
+
+class App extends Component  {
+
+    state = {
+        posts: {},
+        // myUsers:{},
+        user: null
+    }
+
+
+    render()    {
+      
+        return(
+        <div className="app">
+            <p>hi</p>
+            
+        </div>
+    )
+    }
+}
+export default App
