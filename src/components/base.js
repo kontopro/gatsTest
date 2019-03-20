78e1e6811b8388c5fa186a72242bc20034e17021
kontopro <konteap@gmail.com>
2019-03-20 17:05:21 +0200

    hack building

--- src/components/base.js
+++ src/components/base.js
@@ -2,14 +2,16 @@ import Rebase from "re-base";
 import firebase from "firebase/app";
 import 'firebase/database';
 
-const firebaseApp = firebase.initializeApp({
-  apiKey: process.env.GATSBY_API_KEY,
-  authDomain: process.env.GATSBY_AUTH_DOMAIN,
-  databaseURL: process.env.GATSBY_DATABASE_URL
-});
+  
+const firebaseApp = (typeof window !== 'undefined')?
+  firebase.initializeApp({
+    apiKey: process.env.GATSBY_API_KEY,
+    authDomain: process.env.GATSBY_AUTH_DOMAIN,
+    databaseURL: process.env.GATSBY_DATABASE_URL
+  }):{};
 
-const base = Rebase.createClass(firebaseApp.database());
-
+const base = (typeof window !== 'undefined')?
+  Rebase.createClass(firebaseApp.database()):{};
 // This is a named export
 export { firebaseApp };
 
