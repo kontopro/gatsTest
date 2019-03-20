825b87fdc2f57138a6d6628a93e7a09728208f15
kontopro <konteap@gmail.com>
2019-03-20 20:20:15 +0200

    Update app.js

--- src/pages/app.js
+++ src/pages/app.js
@@ -13,13 +13,7 @@ class App extends Component  {
         user: null
     }
     
-    authenticate = provider => {
-    const authProvider = new firebase.auth[`${provider}AuthProvider`]();
-    firebaseApp
-      .auth()
-      .signInWithPopup(authProvider)
-      .then(this.authHandler);
-    };
+    
 
     authHandler = async authData => {
       await this.setState({user: authData.user});
@@ -40,6 +34,13 @@ class App extends Component  {
               this.authHandler({ user });
             }
         });
+        authenticate = provider => {
+          const authProvider = new firebase.auth[`${provider}AuthProvider`]();
+          firebaseApp
+            .auth()
+            .signInWithPopup(authProvider)
+            .then(this.authHandler);
+          };
       }
       
     componentWillUnmount() {
