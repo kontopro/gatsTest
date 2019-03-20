0eaaa07aeb8e15a24cddd4844da967cd720c1c00
kontopro <konteap@gmail.com>
2019-03-20 22:23:17 +0200

    Update app.js

--- src/pages/app.js
+++ src/pages/app.js
@@ -33,6 +33,7 @@ class App extends Component  {
     logout = async () => {
       await firebase.auth().signOut();
       this.setState({ user: null });
+      base.removeBinding(this.postRef);
     };
 
     componentDidMount() {
