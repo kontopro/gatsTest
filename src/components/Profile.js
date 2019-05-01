eee350625a49257dcb77f55eae5a705414a37f14
kontopro <konteap@gmail.com>
2019-05-01 16:51:51 +0300

    app-profile

--- src/components/Profile.js
+++ src/components/Profile.js
@@ -3,11 +3,17 @@ import React from "react"
 const Profile = (props) => (
     <div className='app-profile'>
         <p>Profile component</p>
-        {Object.keys(props.userposts).map(x => 
-        <div key={x}>
-            <p>{x}</p>  
-        </div>      
-            
+        <div className='app-profile-create-post'>
+            <p>Create new post</p>
+            <button>new post</button>
+        </div>
+        {Object.keys(props.userposts[props.uid]).map(x => 
+        <div key={x} className='app-profile-postcard global-smooth-card'>
+            <img alt='title' width='250' src={`${props.posts[x].fimage}`}/>
+            <p>{props.posts[x].title}</p>
+            <button>Edit</button> 
+            <button>Delete</button> 
+        </div>
             )}
     </div>
 )
