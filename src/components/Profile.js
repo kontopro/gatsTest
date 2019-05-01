90ab42ff96d9313a096e748128a6b5a29214af4a
kontopro <konteap@gmail.com>
2019-05-01 18:49:22 +0300

    Update Profile.js
    delete uid from profile, to solve refresh problems

--- src/components/Profile.js
+++ src/components/Profile.js
@@ -1,21 +1,31 @@
-import React from "react"
+import React, {useState, useEffect} from "react";
 
-const Profile = (props) => (
+
+function Profile(props) {
+
+    const [posts, setPosts] = useState(props.posts)
+    const [userposts, setUserposts] = useState(props.userposts)
+    useEffect(() => {
+        setPosts(props.posts)
+        setUserposts(props.userposts)
+    },[props])
+    if (props.posts){
+    return(
     <div className='app-profile'>
         <p>Profile component</p>
         <div className='app-profile-create-post'>
             <p>Create new post</p>
             <button>new post</button>
         </div>
-        {Object.keys(props.userposts[props.uid]).map(x => 
+        {Object.keys(userposts).map(x => 
         <div key={x} className='app-profile-postcard global-smooth-card'>
-            <img alt='title' width='250' src={`${props.posts[x].fimage}`}/>
-            <p>{props.posts[x].title}</p>
+            <img alt='title' width='250' src={`${posts[x].fimage}`}/>
+            <p>{posts[x].title}</p>
             <button>Edit</button> 
             <button>Delete</button> 
         </div>
             )}
     </div>
-)
-
+)}else{return (<div><p>loading...</p></div>)}
+}
 export default Profile
